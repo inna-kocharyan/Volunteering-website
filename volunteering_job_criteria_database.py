f6230d5fab63d5c6b0a096526b9859f7e8218261
TaronAndranyan <62055527+TaronAndranyan@users.noreply.github.com>
2020-03-19 21:07:39 +0400

    Create volunteering_job_criteria_database.py

---
+++ volunteering_job_criteria_database.py
@@ -0,0 +1,9 @@
+Volunteering_Opportunity_1 = {
+  "minimum age" : input("Please specify your minimum age criteria"),
+  "maximum age" : input("Please specify the maximum age"),
+  "minimum experience in years" : input("Please specify the minimum experience in years"),
+  "Knowledge of Excel" : input("Do you require knowledge of excel for this job"),
+  "Fluency level of English" : input("Please specify the level of fluency in English"),
+  "Fluency level of Russian" : input("Please specify the level of fluency in Russian")
+}
+print(Volunteering_Opportunity_1)
