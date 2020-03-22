c1db80ab95326cc134a9e74324ebda4c3709a13c
TaronAndranyan <62055527+TaronAndranyan@users.noreply.github.com>
2020-03-22 21:14:53 +0400

    Update volunteering_job_criteria_database.py

--- volunteering_job_criteria_database.py
+++ volunteering_job_criteria_database.py
@@ -1,9 +1,37 @@
 Volunteering_Opportunity_1 = {
-  "minimum age" : input("Please specify your minimum age criteria"),
-  "maximum age" : input("Please specify the maximum age"),
-  "minimum experience in years" : input("Please specify the minimum experience in years"),
-  "Knowledge of Excel" : input("Do you require knowledge of excel for this job"),
-  "Fluency level of English" : input("Please specify the level of fluency in English"),
-  "Fluency level of Russian" : input("Please specify the level of fluency in Russian")
+  "minimum age" : 18,
+  "maximum age" : 20,
+  "experience" : 3,
+  "excel" : "yes",
+  "English" : "beginner",
+  "Russian" : "beginner"
 }
-print(Volunteering_Opportunity_1)
+Volunteering_Opportunity_2 = {
+  "minimum age" : 16,
+  "maximum age" : 19,
+  "experience" : 1,
+  "excel" : "no",
+  "English" : "beginner",
+  "Russian" : "intermediate"
+}
+Person1 = {
+  "age" : input("Please incert your age\n"),
+  "experience" : input("Please specify your experience in years\n"),
+  "excel" : input("Can you use excel?\n"),
+  "English" : input ("Please specify your level of English\n"),
+  "Russian" : input("Please specify your level of Russian\n")
+}
+if (int(Volunteering_Opportunity_1["minimum age"]) <= int(Person1["age"]) <= int(Volunteering_Opportunity_1["maximum age"])):
+  if (int(Person1["experience"]) >= int(Volunteering_Opportunity_1["experience"])):
+   if (Person1["excel"] == Volunteering_Opportunity_1["excel"]):
+     if (Person1["English"] == Volunteering_Opportunity_1 ["English"]):
+       if (Person1["Russian"] == Volunteering_Opportunity_1["Russian"]):
+         print("Volunteering Opportunity 1 Matches you")
+else:
+  if(int(Volunteering_Opportunity_2["minimum age"]) <= int(Person1["age"]) <= int(Volunteering_Opportunity_2["maximum age"])):
+    if (int(Person1["experience"]) >= int(Volunteering_Opportunity_2["experience"])):
+      if (Person1["excel"] == Volunteering_Opportunity_2["excel"]):
+        if (Person1["English"] == Volunteering_Opportunity_2 ["English"]):
+          if (Person1["Russian"] == Volunteering_Opportunity_2["Russian"]):
+            print ("Volunteering Opportunity 2 Matches you")
+            
