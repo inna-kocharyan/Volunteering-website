dff82d5edb6fe0b4a9326564d19ef96b155ea710
TaronAndranyan <62055527+TaronAndranyan@users.noreply.github.com>
2020-03-25 22:49:48 +0400

    Update matching.py

--- matching.py
+++ matching.py
@@ -1,37 +1,32 @@
-Volunteering_Opportunity_1 = {
-  "minimum age" : 18,
-  "maximum age" : 20,
-  "experience" : 3,
-  "excel" : "yes",
-  "English" : "beginner",
-  "Russian" : "beginner"
-}
-Volunteering_Opportunity_2 = {
-  "minimum age" : 16,
-  "maximum age" : 19,
-  "experience" : 1,
-  "excel" : "no",
-  "English" : "beginner",
-  "Russian" : "intermediate"
-}
 Person1 = {
-  "age" : input("Please incert your age\n"),
-  "experience" : input("Please specify your experience in years\n"),
-  "excel" : input("Can you use excel?\n"),
-  "English" : input ("Please specify your level of English\n"),
-  "Russian" : input("Please specify your level of Russian\n")
-}
-if (int(Volunteering_Opportunity_1["minimum age"]) <= int(Person1["age"]) <= int(Volunteering_Opportunity_1["maximum age"])):
-  if (int(Person1["experience"]) >= int(Volunteering_Opportunity_1["experience"])):
-   if (Person1["excel"] == Volunteering_Opportunity_1["excel"]):
-     if (Person1["English"] == Volunteering_Opportunity_1 ["English"]):
-       if (Person1["Russian"] == Volunteering_Opportunity_1["Russian"]):
-         print("Volunteering Opportunity 1 Matches you")
-else:
-  if(int(Volunteering_Opportunity_2["minimum age"]) <= int(Person1["age"]) <= int(Volunteering_Opportunity_2["maximum age"])):
-    if (int(Person1["experience"]) >= int(Volunteering_Opportunity_2["experience"])):
-      if (Person1["excel"] == Volunteering_Opportunity_2["excel"]):
-        if (Person1["English"] == Volunteering_Opportunity_2 ["English"]):
-          if (Person1["Russian"] == Volunteering_Opportunity_2["Russian"]):
-            print ("Volunteering Opportunity 2 Matches you")
-            
+"age" : input("Please incert your age\n"),
+"experience" : input("Please specify your experience in years\n"),
+"excel" : input("Can you use excel?\n"),
+"English" : input ("Please specify your level of English\n"),
+"Russian" : input("Please specify your level of Russian\n")}
+student_success_index = 0
+opportunities = 4
+count = 0
+import csv
+with open('opportunities.csv', 'r') as csv_file:
+ csv_reader = csv.reader(csv_file)
+ for line in csv_reader:
+   if line[1] == Person1["age"]:
+     student_success_index += 1
+   if line[3] <= Person1["experience"]:
+     student_success_index += 1
+   if line [4] == Person1["excel"]:
+     student_success_index += 1
+   if line[5] == Person1["English"]:
+     student_success_index += 1
+   if line[6] == Person1["Russian"]:
+     student_success_index += 1
+ while count < opportunities and int(student_success_index) >= int(line[7]):
+    print(line[0]) 
+    count += 1 
+    
+         
+
+
+
+
