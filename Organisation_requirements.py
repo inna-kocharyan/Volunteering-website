86bd16ee3397b5c78eb086b236fb6ea62ce731b6
alex16zar <62423985+alex16zar@users.noreply.github.com>
2020-03-25 21:03:45 +0400

    Update Organisation_requirements.py

--- Organisation_requirements.py
+++ Organisation_requirements.py
@@ -1,10 +1,10 @@
 #to check the age that the company wants#
-age_min = int(input("Please enter the minimum age requered for this job\n"))
+age_min = int(input("Please enter the minimum age required for this job\n"))
 if age_min <= 15: 
     print(" this is under the state age limit")
 else:
     print("")
-age_max = int(input("Please enter the maximum age requered for this job\n"))
+age_max = int(input("Please enter the maximum age required for this job\n"))
 if age_max >= 60: 
     print(" Not possible")
 else:
@@ -19,7 +19,7 @@ while True:
         print("Too much info - keep it shorter!")
 while True:
 	try:
-		number = int(input("what's the minimum years of work expiriance  that is required\n "))
+		number = int(input("what's the minimum years of work experience  that is required\n "))
 		assert(number > 0), 'Number must be bigger than 0'
 		break
 	except:
@@ -47,12 +47,20 @@ for key in language_level.keys():
 
 print(language_level)
 
-#this is the last part after the whole coad is writen#
-input(" please insert any website links (*not requiered*)\n")
-
 #when dose the opertunity start and end##
 import datetime
-inputDate = input("Enter the date in format 'dd/mm/yy' : ")
+inputDate = input("when is the opertunity going to start 'dd/mm/yy' : ")
+day,month,year = inputDate.split('/')
+isValidDate = True
+try :
+    datetime.datetime(int(year),int(month),int(day))
+except ValueError :
+    isValidDate = False
+if(isValidDate) :
+    print ("Input date is valid ..")
+else :
+    print ("Input date is not valid..")
+inputDate = input("when is the opertunity going to end 'dd/mm/yy' : ")
 day,month,year = inputDate.split('/')
 isValidDate = True
 try :
@@ -64,10 +72,24 @@ if(isValidDate) :
 else :
     print ("Input date is not valid..")
 #if interwiew is requered #
-interview = (input("is an interwivew requiered? (y/n)\n"))
+interview = (input("is an interview required? (y/n)\n"))
 if interview=='No'or 'Yes' or 'no' or 'yes' :
     print("")
 else:
     print("please enter a valid answere")
         #ask the location #
-input("where is the job opertunity going to be held\n")
+input("where is the job opportunity going to be held\n")
+#name of the comapny#
+input("what is the company/orgnizations called?\n")
+#the benifits of the job#
+while True:
+    answer = input("What will the participants gain from the opportunity?\n")
+    if len(answer) <= 100:
+        break
+    else:
+        print("Too much info - keep it shorter!")
+
+    #the duration of the program#
+input("How long is the opportunity giong to last?\n")
+#links to website#
+input("please enter your website link\n")
