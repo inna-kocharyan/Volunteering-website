a6299db278ec3370805c3c5fba132428860cbf79
TaronAndranyan <62055527+TaronAndranyan@users.noreply.github.com>
2020-03-26 00:30:05 +0400

    Update matching_volunteer_input.py

--- matching_volunteer_input.py
+++ matching_volunteer_input.py
@@ -4,29 +4,12 @@ Person1 = {
 "excel" : input("Can you use excel?\n"),
 "English" : input ("Please specify your level of English\n"),
 "Russian" : input("Please specify your level of Russian\n")}
-student_success_index = 0
-opportunities = 4
-count = 0
+
 import csv
-with open('opportunities.csv', 'r') as csv_file:
- csv_reader = csv.reader(csv_file)
- for line in csv_reader:
-   if line[1] == Person1["age"]:
-     student_success_index += 1
-   if line[3] <= Person1["experience"]:
-     student_success_index += 1
-   if line [4] == Person1["excel"]:
-     student_success_index += 1
-   if line[5] == Person1["English"]:
-     student_success_index += 1
-   if line[6] == Person1["Russian"]:
-     student_success_index += 1
- while count < opportunities and int(student_success_index) >= int(line[7]):
-    print(line[0]) 
-    count += 1 
-    
-         
+opp_list = ["Volunteering_Opportunity", "age", "experienc", "excel", "English", "Russian"]
 
-
-
-
+with open ("opportunities.csv") as csvfile:
+  reader = csv.DictReader(csvfile)
+  for opp in reader:
+    if int(opp["age"]) <= int(Person1["age"]) and int(opp["experience"]) <= int(Person1["experience"]) and opp["English"] == Person1["English"] and opp["Russian"] == Person1["Russian"]:
+      print ("Congrats you match to the volunteering opportunity", opp["Volunteering_Opportunity"])
