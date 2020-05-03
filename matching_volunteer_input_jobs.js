7becf469c47b970298d1b02679820fe16a905ea9
TaronAndranyan <62055527+TaronAndranyan@users.noreply.github.com>
2020-05-04 01:06:43 +0400

    Create matching_volunteer_input_jobs.js

---
+++ matching_volunteer_input_jobs.js
@@ -0,0 +1,19 @@
+var jobs = {
+"opportunities": [
+{"Name": 'opportunity 1',
+"required_age": 18,
+"required_experience": 3,
+"excel" : 1,
+"English": 2,
+"Russian": 2
+},
+{
+"Name": 'opportunity 2',
+"required_age": 16,
+"required_experience": 0,
+"excel" : 2,
+"English": 2,
+"Russian": 2
+}
+]
+}
