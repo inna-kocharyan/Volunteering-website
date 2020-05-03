e953d1c69d9ca3f726c6afe43866501db9017773
inna-kocharyan <62055283+inna-kocharyan@users.noreply.github.com>
2020-05-03 13:05:49 +0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,94 @@
+var volunteers_list =
+{  "volunteers": [
+{"First Name": "Jake",
+"Last Name": "Black",
+"Age": 23,
+"English": 5,
+"Armenian": 1,
+"German": 4,
+"Russian": 3
+},
+{
+"First Name": "Anna",
+"Last Name": "Green",
+"Age": 20,
+"English": 5,
+"Armenian": 3,
+"German": 3,
+"Russian": 1
+},
+{
+"First Name": "Alison ",
+"Last Name": "Rowland",
+"Age": 17,
+"English": 2,
+"Armenian": 2,
+"German": 1,
+"Russian": 1
+},
+{
+"First Name": "Charles ",
+"Last Name": "Davenport",
+"Age": 19,
+"English": 3,
+"Armenian": 1,
+"German": 4,
+"Russian": 4
+},
+{
+"First Name": "Theo ",
+"Last Name": "Carlson",
+"Age": 23,
+"English": 4,
+"Armenian": 5,
+"German": 1,
+"Russian": 3
+},
+{
+"First Name": "Ryan",
+"Last Name": "Brown",
+"Age": 19,
+"English": 3,
+"Armenian": 1,
+"German": 5,
+"Russian": 5
+},
+{
+"First Name": "Eleanor ",
+"Last Name": "Bloom",
+"Age": 19,
+"English": 1,
+"Armenian": 3,
+"German": 2,
+"Russian": 4
+},
+{
+"First Name": "Stephen ",
+"Last Name": "McNeil",
+"Age": 17,
+"English": 3,
+"Armenian": 3,
+"German": 3,
+"Russian": 2
+},
+{
+"First Name": "Vladimir",
+"Last Name": "Ivanov",
+"Age": 25,
+"English": 2,
+"Armenian": 1,
+"German": 1,
+"Russian": 1
+},
+{
+"First Name": "Maria",
+"Last Name": "Sakharova",
+"Age": 20,
+"English": 4,
+"Armenian": 1,
+"German": 3,
+"Russian": 3
+}
+]
+}
+ 
